622dde51f830bdb86fd23b0dd0b97be22efa77c9
evgenij89 <evgenij.dutov@privatbank.ua>
2021-10-25 17:59:45 +0300

    init commit

--- src/com/pb/dutov/hw3/Bingo.java
+++ src/com/pb/dutov/hw3/Bingo.java
@@ -6,21 +6,30 @@ public class Bingo {
         int counter = 0;
         Random random = new Random();
         int x = random.nextInt(101);
-        //System.out.println("Текущее число: " + x);
+        System.out.println("Текущее число: " + x);
         Scanner scan = new Scanner(System.in);
         int y;
             while (counter < 100) {
             counter++;
-            System.out.println("Введите число:");
+            System.out.println("\nВведите число от 0 100\nВведите 101 чтобы закончить игру:");
             y = scan.nextInt();
             if (x == y)  {
             System.out.println("Поздравляем, Вы угадали с " + counter + " попытки");
             break;
-        } else if (x > y) {
-            System.out.print("Задуманное число больше, чем Вы предполагаете. ");
+        } else if (x > y && (y >=0 && y<=100)) {
+            System.out.println("Задуманное число больше, чем Вы предполагаете. ");
         }
-            else {
-            System.out.print("Задуманное число меньше, чем Вы предполагаете. ");
+        else if (x < y && (y >=0 && y<=100)) {
+                System.out.println("Задуманное число меньше, чем Вы предполагаете. ");
+        }
+        else if (y == 101) {
+            System.out.println ("Игра окончена");
+            break;
+        }
+        else {
+            System.out.print("Задуманное число должно быть от 0 до 100\nИгра окончена ");
+            break;
+
         }}
     }
 
